3e7fef28517ddc67ab8ec05ad0d7d25ba3d32b36
王宁 <dot.n.wang@faxmail.com>
2020-07-29 22:06:53 +0800

    update

--- src/main/java/com/example/Test.java
+++ src/main/java/com/example/Test.java
@@ -2,6 +2,9 @@ package com.example;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Test {
 
@@ -26,5 +29,42 @@ public class Test {
             }
             System.out.println(temp);
         }
+
+        int[] arr={1,3,53,4,2};
+        List<Integer> list= new ArrayList<Integer>(5);
+        Arrays.sort(arr);
+        System.out.println(arr);
+        String time="7:01:01PM";
+        if(time.endsWith("AM")){
+            System.out.println(time.substring(0,time.indexOf("AM")));
+        }else {
+            String temp=time.substring(0,time.indexOf("PM"));
+            String[] tempArr=temp.split(":");
+            String result=(Integer.parseInt(tempArr[0])+12)+":"+tempArr[1]+":"+tempArr[2];
+            System.out.println(result);
+        }
+
+        countApplesAndOranges(37455 ,87275,);
+
+    }
+
+    static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+        int resultS=0;
+        int resultT=0;
+        for(int i=0,length=apples.length;i<length;i++){
+            long temp=(long)a+apples[i];
+            if(temp>=s&&temp<=t){
+                resultS++;
+            }
+        }
+        for(int i=0,length=oranges.length;i<length;i++){
+            long temp=(long)b+oranges[i];
+            if(temp>=s&&temp<=t){
+                resultT++;
+            }
+        }
+        System.out.println(resultS);
+        System.out.println(resultT);
+
     }
 }
